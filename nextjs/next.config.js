ed1c75d395782d2da3ab8f27b88b641e673aba81
Emerik Aji <emerikaji@gmail.com>
2024-12-21 01:06:13 +0000

    add app.yaml for deployment

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/api/ldf',
-        destination: `${process.env.API_BASE_URL}/api/ldf`,
-      },
-    ];
-  },
 };
